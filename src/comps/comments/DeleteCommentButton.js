6bf5760cbd86f373078f7b1a59773cab2871182f
IVAN <ivanbatur5g@gmail.com>
2020-12-14 19:35:32 +0100

    Returned the tooltip for deletecommentdialog, proejct donegit status

--- src/comps/comments/DeleteCommentButton.js
+++ src/comps/comments/DeleteCommentButton.js
@@ -3,16 +3,19 @@ import IconButton from "@material-ui/core/IconButton"
 import {useState} from "react"
 import DeleteIcon from '@material-ui/icons/Delete';
 import DeleteCommentDialog from "./DeleteCommentDialog"
-// import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from '@material-ui/core/Tooltip';
 import {useUser} from "../../context/context"
 const DeleteCommentButton = ({userId, commentId, postId}) => {
     const [open, setOpen] = useState(false)
     const {user} = useUser()
     const markup = user.uid === userId ? (
-        <IconButton aria-label="delete" onClick={() => setOpen(!open)} className="delete-comment">
+        <Tooltip title="Delete">
+            <IconButton aria-label="delete" onClick={() => setOpen(!open)} className="delete-comment">
             <DeleteIcon />
             <DeleteCommentDialog postId={postId} open={open} setOpen={setOpen} commentId={commentId}/>
         </IconButton>
+        </Tooltip>
+        
   )
      :
       null
